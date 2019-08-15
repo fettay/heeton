a88e2a27195a477dfe5ee6db491a371ad2fef891
jouazan <jouazan@outbrain.com>
2019-08-15 22:57:27 +0300

    correct variable names

--- server/nlp.py
+++ server/nlp.py
@@ -6,16 +6,17 @@ df_freq = pd.read_csv('stopwords.csv')
 df_freq.columns = ['freq', 'pointer', 'word', 'pos']
 df_freq = df_freq[['word', 'pos', 'freq', 'pointer']]
 
+
 def score_word(word):
-    freq = df.loc[df.word == word, 'freq']
+    freq = df_freq.loc[df_freq.word == word, 'freq']
     if len(freq) == 0:
         return -1
     else:
         return 1 / np.log(freq.iloc[0])**2
-        
+
+
 def get_hardwords(text):
     toks = list(tok.tokenize(text))
     res = [(tok[1], score_word(tok[1])) for tok in toks]
     return [r[0] for r in res if r[1] > 0.05]
-    
-    
+
